4576d9dd536832c3a95eb5b88b277d6275421d64
Rodrigo Lima Barbosa12 <rodrigo.limabarbosa@upe.br>
2025-09-08 15:36:24 -0300

    Versão Private
    Desafio II: Sistema de Controle de Livros
    Como o encapsulamento pode ser aplicado para criar um sistema de controle de livros em uma biblioteca? Desenvolver uma classe Livro com os seguintes requisitos:
    
    . Atributos titulo, autor e emprestado (boolean) devem ser privados
    . Crie métodos especiais emprestar() e devolver() para controlar o status do livro, garantindo que a lógica de empréstimo/devolução seja consistente.

--- Livro.java
+++ Livro.java
@@ -1,49 +1,46 @@
 public class Livro {
-    String titulo, autor;
-        int paginas;
-        boolean emprestado = false;
+    private String titulo;
+    private String autor;
+    private boolean emprestado = false;
 
-        public String getTitulo(){
-            return titulo;
-        }
-        public void setTitulo(String titulo){
-            this.titulo = titulo;
-        }
-        public String getAutor(){
-            return autor;
-        }
-        public void setAutor(String autor){
-            this.autor = autor;
-        }
-        public int getPaginas(){
-            return paginas;
-        }
-        public void setPaginas(int paginas){
-            this.paginas = paginas;
-        }
-        public void emprestar(){
-            if (!emprestado) {
-                emprestado = true;
-                System.out.println("O Livro foi emprestado");
-            }
-            else {
-                System.out.println("O Livro está disponível para locação");
-            }
-        }
-        public void devolver(){
-            if (emprestado) {
-                emprestado = false;
-                System.out.println("Livro devolvido");
-            }
-            else {
-                System.out.println("Este livro não foi emprestado");
-            }
-        }
-        public void exibirInformacoes(){
-            System.out.println("\n--- Dados do Livro ---");
-            System.out.println("Titulo: " + titulo);
-            System.out.println("Autor: " + autor);
-            System.out.println("Paginas: " + paginas);
-            System.out.println("Status: " + (emprestado ? "Emprestado" : "Disponível"));
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void emprestar() {
+        if (!emprestado) {
+            emprestado = true;
+            System.out.println("O livro foi emprestado com sucesso.");
+        } else {
+            System.out.println("O livro já está emprestado.");
         }
     }
+
+    public void devolver() {
+        if (emprestado) {
+            emprestado = false;
+            System.out.println("O livro foi devolvido com sucesso.");
+        } else {
+            System.out.println("Este livro não está emprestado.");
+        }
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("\n--- Dados do Livro ---");
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Status: " + (emprestado ? "Emprestado" : "Disponível"));
+    }
+}
